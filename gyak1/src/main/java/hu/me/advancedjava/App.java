23172704c5cfa5133f9333451c124dc3a491b962
mleleszi <mleleszi@icloud.com>
2022-02-17 09:01:35 +0100

    added gitignore

---
+++ gyak1/src/main/java/hu/me/advancedjava/App.java
@@ -0,0 +1,13 @@
+package hu.me.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
